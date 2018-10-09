ba42af99133ad63dafbda7596d9523e1e3f06c39
luoqiangwei <luoqiangwei@live.cn>
2018-10-09 16:12:00 +0800

    css, js, img static source can used.

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from . import views
 from django.urls import path
+# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url('^$', views.index),
 ]
+
+# urlpatterns += staticfiles_urlpatterns()
