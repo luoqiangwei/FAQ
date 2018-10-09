460f6bdcc8bcd7e6599dd40721977bcd3eb28ea8
luoqiangwei <luoqiangwei@live.cn>
2018-10-09 13:13:41 +0800

    fail

--- mark/models.py
+++ mark/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from user.models import UserInfo
 
 # Create your models here.
+class MarkRecodeInfo(models.Model):
+    uid = models.ForeignKey(UserInfo)
+    mscore = models.IntegerField()
+    mdate = models.DateTimeField(auto_now_add=True)
--- user/models.py
+++ user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class UserInfo(models.Model):
+    uuser = models.CharField(max_length=100)
+    upass = models.CharField(max_length=100)
+    uaccode = models.CharField(max_length=4)
+    uaccodedate = models.DateTimeField(auto_now=True)
